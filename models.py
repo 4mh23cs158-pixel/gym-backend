6c46fd5205f79a73ad7aab3abc24ede237bd560a
Snigdha K P <4MH23CS158@gmail.com>
2026-03-02 16:00:33 +0530

    posrt

--- models.py
+++ models.py
@@ -2,6 +2,11 @@ from db import Base
 from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Text
 from sqlalchemy.orm import relationship
 from datetime import datetime
+from sqlalchemy import Column, Integer, String, Float, Date, ForeignKey
+from sqlalchemy.orm import relationship
+from datetime import date
+from db import engine
+
 
 class User(Base):
     __tablename__ = "users"
@@ -10,3 +15,20 @@ class User(Base):
     name = Column(String)
     email = Column(String, unique=True)
     hashed_password = Column(String)
+    meals = relationship("Meal", back_populates="user", cascade="all, delete")
+
+class Meal(Base):
+    __tablename__ = "meals"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    calories = Column(Integer)
+    protein = Column(Integer)
+    carbs = Column(Integer)
+    fat = Column(Integer)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    created_at = Column(DateTime, default=datetime.utcnow)
+    user = relationship("User", back_populates="meals")
+
+
+Base.metadata.create_all(bind=engine)
